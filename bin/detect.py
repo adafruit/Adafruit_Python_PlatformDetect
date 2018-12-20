3ccd25d88613dcae4c2a7044ab5629efd713cc66
Brennen Bearnes <brennen@adafruit.com>
2018-12-20 16:23:49 -0700

    add any_linux property check to bin/detect.py

--- bin/detect.py
+++ bin/detect.py
@@ -12,6 +12,7 @@ print("Is this a Pi 3B+?", detector.board.RASPBERRY_PI_3B_PLUS)
 print("Is this a 40-pin Raspberry Pi?", detector.board.RASPBERRY_PI_3B_PLUS)
 print("Is this a BBB?", detector.board.BEAGLEBONE_BLACK)
 print("Is this an Orange Pi PC?", detector.board.ORANGE_PI_PC)
+print("Is this a Linux system?", detector.board.any_linux)
 print("Is this a generic Linux PC?", detector.board.GENERIC_LINUX_PC)
 
 if detector.board.any_raspberry_pi:
