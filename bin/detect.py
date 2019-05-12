4f0e22eead29b11941c505d93878648121c8896e
ladyada <limor@ladyada.net>
2019-05-11 22:57:02 -0400

    add coral edge tpu dev board code name "Phanbell"

--- bin/detect.py
+++ bin/detect.py
@@ -13,6 +13,7 @@ print("Is this a 40-pin Raspberry Pi?", detector.board.any_raspberry_pi_40_pin)
 print("Is this a BBB?", detector.board.BEAGLEBONE_BLACK)
 print("Is this an Orange Pi PC?", detector.board.ORANGE_PI_PC)
 print("Is this a Giant Board?", detector.board.GIANT_BOARD)
+print("Is this a Coral Edge TPU?", detector.board.CORAL_EDGE_TPU_DEV)
 print("Is this an embedded Linux system?", detector.board.any_embedded_linux)
 print("Is this a generic Linux PC?", detector.board.GENERIC_LINUX_PC)
 
