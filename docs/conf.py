02003debe9fb376ebfd04c2433739ada0df829bf
Melissa LeBlanc-Williams <melissa@adafruit.com>
2020-04-20 17:21:57 -0700

    Updated RTD conf

--- docs/conf.py
+++ docs/conf.py
@@ -3,7 +3,7 @@
 import os
 import sys
 
-sys.path.insert(0, os.path.abspath("../adafruit_platformdetect"))
+sys.path.insert(0, os.path.abspath(".."))
 
 # -- General configuration ------------------------------------------------
 
