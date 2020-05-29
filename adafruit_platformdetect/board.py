d0e22d04d6e942b095e27cfd0a66050b9efdd6ed
BlitzCityDIY <blitzcitydiy@gmail.com>
2020-05-29 15:25:32 -0400

    Updating pylint
    fixing pylint errors

--- adafruit_platformdetect/board.py
+++ adafruit_platformdetect/board.py
@@ -421,7 +421,7 @@ class Board:
 
     def any_asus_tinker_board(self):
         """Check to see if the current board is an ASUS Tinker Board"""
-        return self.id in boards._ASUS_Tinker_Board
+        return self.ASUS_Tinker_Board
 
     @property
     def any_embedded_linux(self):
