111daa078b5647453a55d83edd0ef9c317e05f21
Daniel Manla <60765275+dmanla@users.noreply.github.com>
2020-07-23 15:14:21 -0400

    Add PineH64 to board detection

--- adafruit_platformdetect/board.py
+++ adafruit_platformdetect/board.py
@@ -115,6 +115,8 @@ class Board:
             board_id = self._pynq_id()
         elif chip_id == chips.A64:
             board_id = self._pine64_id()
+        elif chip_id == chips.H6:
+            board_id = self._pine64_id()
         elif chip_id == chips.A33:
             board_id = self._clockwork_pi_id()
         elif chip_id == chips.RK3308:
@@ -233,6 +235,8 @@ class Board:
             board = boards.ORANGE_PI_PC_PLUS
         if board_value == "pinebook-a64":
             board = boards.PINEBOOK
+        if board_value == "pineH64":
+            board = boards.PINEH64
         if board_value == "orangepi2":
             board = boards.ORANGE_PI_2
 
@@ -280,6 +284,8 @@ class Board:
         board = None
         if "pine64" in board_value.lower():
             board = boards.PINE64
+        elif "pine h64" in board_value.lower():
+            board = boards.PINEH64
         elif "pinebook" in board_value.lower():
             board = boards.PINEBOOK
         elif "pinephone" in board_value.lower():
