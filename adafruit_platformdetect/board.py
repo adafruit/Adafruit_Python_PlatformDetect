adafruit/Adafruit_Python_PlatformDetect

commit e3fede2f5c8ed0a2d3e78f6f3323ba87d5ac6d17

Convert _JETSON_IDS to a tuple
This will allow us to control the order that each entry is checked,
whereas the current dictionary is unordered. This will be useful when
adding support for future very similar boards.

adafruit_platformdetect/board.py
@@ -280,7 +280,7 @@ class Board:
         if not compatible:
             return None
         compats = compatible.split("\x00")
-        for board_id, board_compats in boards._JETSON_IDS.items():
+        for board_id, board_compats in boards._JETSON_IDS:
             if any(v in compats for v in board_compats):
                 return board_id
         return None

adafruit_platformdetect/constants/boards.py
@@ -128,25 +128,51 @@ _CORAL_IDS = (CORAL_EDGE_TPU_DEV,)
 
 _PYNQ_IDS = (PYNQ_Z1, PYNQ_Z2)
 
-_JETSON_IDS = {
-    JETSON_TX1: ("nvidia,p2371-2180", "nvidia,jetson-cv"),
-    JETSON_TX2: (
-        "nvidia,p2771-0000",
-        "nvidia,p2771-0888",
-        "nvidia,p3489-0000",
-        "nvidia,lightning",
-        "nvidia,quill",
-        "nvidia,storm",
+_JETSON_IDS = (
+    (
+        JETSON_TX1,
+        (
+            "nvidia,p2371-2180",
+            "nvidia,jetson-cv",
+        ),
     ),
-    JETSON_XAVIER: ("nvidia,p2972-0000", "nvidia,p2972-0006", "nvidia,jetson-xavier"),
-    JETSON_NANO: ("nvidia,p3450-0000", "nvidia,p3450-0002", "nvidia,jetson-nano"),
-    JETSON_NX: (
-        "nvidia,p3509-0000+p3668-0000",
-        "nvidia,p3509-0000+p3668-0001",
-        "nvidia,p3449-0000+p3668-0000",
-        "nvidia,p3449-0000+p3668-0001",
+    (
+        JETSON_TX2,
+        (
+            "nvidia,p2771-0000",
+            "nvidia,p2771-0888",
+            "nvidia,p3489-0000",
+            "nvidia,lightning",
+            "nvidia,quill",
+            "nvidia,storm",
+        ),
     ),
-}
+    (
+        JETSON_XAVIER,
+        (
+            "nvidia,p2972-0000",
+            "nvidia,p2972-0006",
+            "nvidia,jetson-xavier",
+        ),
+    ),
+    (
+        JETSON_NANO,
+        (
+            "nvidia,p3450-0000",
+            "nvidia,p3450-0002",
+            "nvidia,jetson-nano",
+        ),
+    ),
+    (
+        JETSON_NX,
+        (
+            "nvidia,p3509-0000+p3668-0000",
+            "nvidia,p3509-0000+p3668-0001",
+            "nvidia,p3449-0000+p3668-0000",
+            "nvidia,p3449-0000+p3668-0001",
+        ),
+    ),
+)
 
 _RASPBERRY_PI_40_PIN_IDS = (
     RASPBERRY_PI_B_PLUS,

commit 2516a166f473e27ca76f863cd29c9ecbc2b14089

Fix any_jetson_board after _JETSON_IDS type change
Fixes issue 103
Fixes: e3fede2f5c8e ("Convert _JETSON_IDS to a tuple")

adafruit_platformdetect/board.py
@@ -409,7 +409,7 @@ class Board:
     @property
     def any_jetson_board(self):
         """Check whether the current board is any defined Jetson Board."""
-        return self.id in boards._JETSON_IDS
+        return self.id in [v[0] for v in boards._JETSON_IDS]
 
     @property
     def any_sifive_board(self):