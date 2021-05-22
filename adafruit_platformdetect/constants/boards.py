bd054034ba80e3e484b8d78b2fdf6538ca7940cc
Timon <timon@skerutsch.de>
2021-05-22 18:24:09 +0200

    Add rervision code for 8 GB CM4 Lite with WiFi

--- adafruit_platformdetect/constants/boards.py
+++ adafruit_platformdetect/constants/boards.py
@@ -428,7 +428,7 @@ _PI_REV_CODES = {
         "2c03112",
     ),
     RASPBERRY_PI_400: ("c03130",),
-    RASPBERRY_PI_CM4: ("a03140", "b03140", "c03140"),
+    RASPBERRY_PI_CM4: ("a03140", "b03140", "c03140","d03140"),
 }
 
 # Onion omega boards
