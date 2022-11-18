98669f2b796740dccc68ed11a3a490ff7ec374ef
lilin <zgwinli555@163.com>
2022-11-18 03:25:41 +0000

    Modify board_value

--- adafruit_platformdetect/board.py
+++ adafruit_platformdetect/board.py
@@ -468,12 +468,12 @@ class Board:
 
         except FileNotFoundError:
             return None
-            
+
     def _rk3566_id(self) -> Optional[str]:
         """Check what type of rk3566 board."""
         board_value = self.detector.get_device_model()
         board = None
-        if board_value and "LubanCat Zero" in board_value:
+        if board_value and "LubanCat-Zero" in board_value:
             board = boards.LUBANCAT_ZERO
         if board_value and "LubanCat1" in board_value:
             board = boards.LUBANCAT1
