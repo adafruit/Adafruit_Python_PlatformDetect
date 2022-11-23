0161bc8684d11af5d0fc64a776036a03ee5ef43b
Melissa LeBlanc-Williams <melissa@adafruit.com>
2022-11-23 10:59:13 -0800

    Ran pre-commit

--- adafruit_platformdetect/chip.py
+++ adafruit_platformdetect/chip.py
@@ -223,7 +223,7 @@ class Chip:
 
         if self.detector.check_dt_compatible_value("rockchip,rk3568"):
             return chips.RK3568
-        
+
         if self.detector.check_dt_compatible_value("rockchip,rk3568b2"):
             return chips.RK3568B2
 
