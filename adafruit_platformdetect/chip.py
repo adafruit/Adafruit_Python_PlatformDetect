f3d7690876769e46e50b95b805b29cad235ff332
Steve Jeong <steve@how2flow.net>
2023-12-01 01:46:58 +0000

    ODROID-M1: Remove RK3568B2 and Use RK3568 instead
    Signed-off-by: Steve Jeong <steve@how2flow.net>

--- adafruit_platformdetect/chip.py
+++ adafruit_platformdetect/chip.py
@@ -245,9 +245,6 @@ class Chip:
         if self.detector.check_dt_compatible_value("rockchip,rk3568"):
             return chips.RK3568
 
-        if self.detector.check_dt_compatible_value("rockchip,rk3568b2"):
-            return chips.RK3568B2
-
         if self.detector.check_dt_compatible_value("rockchip,rk3588"):
             return chips.RK3588
 
