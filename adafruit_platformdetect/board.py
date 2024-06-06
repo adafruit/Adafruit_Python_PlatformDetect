ce32900a40185bfb2d00775e44f72ff83939325f
Hajime Fujimoto <hajime2008x@yahoo.co.jp>
2024-06-06 20:14:06 +0900

    Check rock5c first

--- adafruit_platformdetect/board.py
+++ adafruit_platformdetect/board.py
@@ -635,10 +635,10 @@ class Board:
             board = boards.ROCK_PI_E
         if self.detector.check_board_name_value() == "ROCK Pi X":
             board = boards.ROCK_PI_X
-        if board_value and "ROCK 5" in board_value.upper():
-            board = boards.ROCK_PI_5
         if board_value and "ROCK 5C" in board_value.upper():
             board = boards.ROCK_PI_5C
+        elif board_value and "ROCK 5" in board_value.upper():
+            board = boards.ROCK_PI_5
         if board_value and "RADXA ROCK 4C+" in board_value.upper():
             board = boards.ROCK_PI_4_C_PLUS
         if board_value and "RADXA ROCK 4SE" in board_value.upper():
