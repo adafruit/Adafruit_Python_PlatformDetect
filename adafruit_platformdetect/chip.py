017cceb73ef4e74bc94227e843c14eb1294e9ab7
Bernhard Bablok <bablokb@gmx.de>
2025-06-14 12:27:20 +0200

    add support for Radxa-X4 via U2IF

--- adafruit_platformdetect/chip.py
+++ adafruit_platformdetect/chip.py
@@ -125,7 +125,7 @@ class Chip:
                     if (
                         (
                             # Raspberry Pi Pico
-                            vendor == 0xCAFE
+                            vendor in (0xCAFE, 0xCAFF)
                             and product == 0x4005
                         )
                         or (
