07a9a2cbcfa0796262f851fd45c5dd3a8edca487
Melissa LeBlanc-Williams <melissa@adafruit.com>
2025-09-27 13:07:32 -0700

    Fix Indiedroid Nova detection

--- adafruit_platformdetect/board.py
+++ adafruit_platformdetect/board.py
@@ -212,7 +212,9 @@ class Board:
                 or self._vicharak_id()
             )
         elif chip_id == chips.RK3588S:
-            board_id = self._orange_pi_id() or self._armbian_id()
+            board_id = (
+                self._orange_pi_id() or self._armbian_id() or self._ameridroid_id()
+            )
         elif chip_id == chips.RYZEN_V1605B:
             board_id = self._udoo_id()
         elif chip_id == chips.PENTIUM_N3710:
